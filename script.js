64ffe54ac2d9024662cf2695e7d5daa0ea05a6ce
BlackCatOfficial 💀 (Python, C++, Lua) <quanvm0501alt1@tutamail.com>
2025-11-16 15:19:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function go(link) {
-  // Sau này bạn chỉ cần thay 'LINK...' bằng link thật
-  window.location.href = link;
+  // Mở liên kết trong một tab/cửa sổ mới
+  // Tham số thứ hai '_blank' quy định việc này
+  window.open(link, '_blank');
 }
+
 const scaleSelect = document.getElementById('scaleSelect');
 scaleSelect.addEventListener('change', (e) => {
-document.body.style.transform = `scale(${e.target.value})`;
-document.body.style.transformOrigin = 'top center';
+  document.body.style.transform = `scale(${e.target.value})`;
+  document.body.style.transformOrigin = 'top center';
 });
-  
